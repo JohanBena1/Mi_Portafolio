92d4cc8a929bd8a54205bc353d0cc14a2b04be8a
johanbena1 <johanbena1@gmail.com>
2023-03-03 09:37:51 -0500

    correcciones responsive

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 
+//funcion de seleccion  
+function seleccionar(link){
+    var opciones = document.querySelectorAll('#links a');
+    opciones[0].className ="";
+    opciones[1].className ="";
+    opciones[2].className ="";
+    opciones[3].className ="";
+    opciones[4].className ="";
+    link.className = "seleccionado";
+
+    //desaparecer menu despues de seleccionar
+    var x = document.getElementById("nav");
+    x.className="";
+}
+//responsive menu
 function responsiveMenu(){
     var x = document.getElementById("nav");
     if(x.className===""){
@@ -6,5 +21,24 @@ function responsiveMenu(){
     }else{
         x.className = "";
     }
+    
+}
 
+window.onscroll = function(){
+    efectoHabilidades()
+};
+
+//animacion barras de skills
+
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distacia_skills = window.innerHeight - skills.getClientRects().top;
+    if (distacia_skills >= 300){
+        document.getElementById("ar").classList.add("barra-progreso1");
+        document.getElementById("hm").classList.add("barra-progreso2");
+        document.getElementById("js").classList.add("barra-progreso3");
+        document.getElementById("pt").classList.add("barra-progreso4");
+        document.getElementById("sql").classList.add("barra-progreso5");
+
+    }
 }
